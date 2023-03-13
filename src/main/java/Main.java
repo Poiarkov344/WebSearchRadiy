d96f7f6f1941247ec426ce0b114d4bd4e47e6a02
Yaroslav <ypayatkov@gmail.com>
2023-03-13 23:29:04 +0100

    Finished With adding Excel and fixing small bugs Everything runs smooth

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,10 @@
+import org.apache.poi.common.usermodel.Hyperlink;
+import org.apache.poi.common.usermodel.HyperlinkType;
+import org.apache.poi.ss.usermodel.CreationHelper;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -7,14 +14,18 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 
+import javax.xml.crypto.Data;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
-
+import java.io.FileOutputStream;
 import java.time.Duration;
 import java.util.Iterator;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws  InterruptedException {
+    public static void main(String[] args) throws InterruptedException, IOException {
 
 
 
@@ -85,6 +96,35 @@ public class Main {
 
         }
 
+
+        //Creating Excel
+
+        XSSFWorkbook workbook = new XSSFWorkbook();
+
+        XSSFSheet sheet = workbook.createSheet("Sheet1");
+
+
+        //Creating names for a table in Excel
+
+        int numRows = 0;
+        int numCols = 7;
+        for (int i = 0; i <= numRows; i++) {
+            XSSFRow row = sheet.createRow(i);
+            for (int j = 0; j < numCols; j++) {
+                XSSFCell cell = row.createCell(j);
+                switch (j) {
+                    case 0 -> cell.setCellValue("№ п/п");
+                    case 1 -> cell.setCellValue("Назва предмету закупівлі");
+                    case 2 -> cell.setCellValue("Найменування Замовника");
+                    case 3 -> cell.setCellValue("Дата оприлюднення");
+                    case 4 -> cell.setCellValue("Кінцевий строк подання тендерних пропозицій");
+                    case 5 -> cell.setCellValue("Очікувана вартість");
+                    case 6 -> cell.setCellValue("Посилання");
+                }
+            }
+        }
+
+
         //Search
         for(int i =0; i<pages; i++){
             Thread.sleep(2000);
@@ -96,7 +136,12 @@ public class Main {
             //function
 
             for(WebElement row : listRows1){
+                numCols =1;
+                XSSFRow ExcelRow = sheet.createRow(++numRows);
+
                 System.out.println(row.getText());
+                XSSFCell cell = ExcelRow.createCell(numCols++);
+                cell.setCellValue(row.getText());
                 row.click();
                 wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector("body")));
                 // table 1
@@ -104,8 +149,10 @@ public class Main {
                 List <WebElement> rows4 = table1.findElements(By.className("col-sm-4"));
                 for(WebElement rows : rows4){
                     if(rows.getText().equals("Найменування:")){
+                        cell = ExcelRow.createCell(numCols++);
                         WebElement Data = driver.findElement(By.className("col-sm-6"));
                         System.out.println(rows.getText().trim() + " " + Data.getText());
+                        cell.setCellValue(Data.getText().trim());
                     }
                 }
 
@@ -121,7 +168,9 @@ public class Main {
                         WebElement element1 = iter1.next();
                         WebElement element2 = iter2.next();
                         if(element1.getText().equals("Дата оприлюднення:")|| element1.getText().equals("Кінцевий строк подання тендерних пропозицій:") || element1.getText().equals("Очікувана вартість:")){
+                            cell = ExcelRow.createCell(numCols++);
                             System.out.println(element1.getText().trim() + " " + element2.getText().trim());
+                            cell.setCellValue(element2.getText().trim());
                         }
                     }
                 }catch (NoSuchElementException e){
@@ -131,6 +180,15 @@ public class Main {
 
                 String url = driver.getCurrentUrl();
                 System.out.println(url);
+
+                cell = ExcelRow.createCell(numCols++);
+                cell.setCellValue(url);
+
+
+                CreationHelper createHelper = workbook.getCreationHelper();
+                Hyperlink hyperlink = createHelper.createHyperlink(HyperlinkType.URL);
+                hyperlink.setAddress(url);
+                cell.setHyperlink((org.apache.poi.ss.usermodel.Hyperlink) hyperlink);
 
 
                 driver.navigate().back();
@@ -142,6 +200,16 @@ public class Main {
             next.click();
 
         }
+
         driver.quit();
+
+        //saving  and closing the file
+        FileOutputStream out = new FileOutputStream("output.xlsx");
+
+        workbook.write(out);
+
+        out.close();
+
+        System.out.println("Excel file created successfully.");
     }
 }
